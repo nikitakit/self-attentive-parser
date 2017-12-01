01cc97a2be11f3b9427e2214fe803264044b0187
Nikita Kitaev <nikitakit@gmail.com>
2017-12-01 01:10:46 -0800

    Initial pytorch port

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ import numpy as np
 
 import evaluate
 import parse
+import parse_pytorch
+import torch
 import trees
 import vocabulary
 
@@ -79,8 +81,23 @@ def run_train(args):
         print_vocabulary("Label", label_vocab)
 
     print("Initializing model...")
-    model = dy.ParameterCollection()
-    if args.parser_type == "top-down":
+    if not args.use_pytorch:
+        model = dy.ParameterCollection()
+    if args.use_pytorch and args.parser_type == "chart":
+        parser = parse_pytorch.ChartParser(
+            tag_vocab,
+            word_vocab,
+            label_vocab,
+            args.tag_embedding_dim,
+            args.word_embedding_dim,
+            args.lstm_layers,
+            args.lstm_dim,
+            args.label_hidden_dim,
+            args.dropout,
+        )
+    elif args.use_pytorch:
+        raise NotImplementedError("This parser type unsupported with pytorch")
+    elif args.parser_type == "top-down":
         parser = parse.TopDownParser(
             model,
             tag_vocab,
@@ -107,7 +124,10 @@ def run_train(args):
             args.label_hidden_dim,
             args.dropout,
         )
-    trainer = dy.AdamTrainer(model)
+    if not args.use_pytorch:
+        trainer = dy.AdamTrainer(model)
+    else:
+        trainer = torch.optim.Adam(parser.parameters())
 
     total_processed = 0
     current_processed = 0
@@ -125,7 +145,8 @@ def run_train(args):
 
         dev_predicted = []
         for tree in dev_treebank:
-            dy.renew_cg()
+            if not args.use_pytorch:
+                dy.renew_cg()
             sentence = [(leaf.tag, leaf.word) for leaf in tree.leaves()]
             predicted, _ = parser.parse(sentence)
             dev_predicted.append(predicted.convert())
@@ -154,7 +175,10 @@ def run_train(args):
             best_dev_model_path = "{}_dev={:.2f}".format(
                 args.model_path_base, dev_fscore.fscore)
             print("Saving new best model to {}...".format(best_dev_model_path))
-            dy.save(best_dev_model_path, [parser])
+            if not args.use_pytorch:
+                dy.save(best_dev_model_path, [parser])
+            else:
+                print("WARNING: saving not implemented when using pytorch")
 
     for epoch in itertools.count(start=1):
         if args.epochs is not None and epoch > args.epochs:
@@ -164,7 +188,8 @@ def run_train(args):
         epoch_start_time = time.time()
 
         for start_index in range(0, len(train_parse), args.batch_size):
-            dy.renew_cg()
+            if not args.use_pytorch:
+                dy.renew_cg()
             batch_losses = []
             for tree in train_parse[start_index:start_index + args.batch_size]:
                 sentence = [(leaf.tag, leaf.word) for leaf in tree.leaves()]
@@ -176,10 +201,16 @@ def run_train(args):
                 total_processed += 1
                 current_processed += 1
 
-            batch_loss = dy.average(batch_losses)
-            batch_loss_value = batch_loss.scalar_value()
-            batch_loss.backward()
-            trainer.update()
+            if not args.use_pytorch:
+                batch_loss = dy.average(batch_losses)
+                batch_loss_value = batch_loss.scalar_value()
+                batch_loss.backward()
+                trainer.update()
+            else:
+                batch_loss = sum(batch_losses) / len(batch_losses)
+                batch_loss_value = float(batch_loss.data.numpy())
+                batch_loss.backward()
+                trainer.step()
 
             print(
                 "epoch {:,} "
@@ -202,22 +233,97 @@ def run_train(args):
                 current_processed -= check_every
                 check_dev()
 
+def dynet_to_pytoch(dynet_parser):
+    spec = dynet_parser.spec
+
+    def map_name(k):
+        if k.startswith("/Parser/_"):
+            num = int(k.split("_")[1])
+            if num == 0:
+                return "tag_embeddings.weight"
+            else:
+                return "word_embeddings.weight"
+        elif k.startswith("/Parser/Feedforward"):
+            num = int(k.split("_")[1])
+            base_idx = (num // 2) * 2
+            role = "weight" if (num % 2 == 0) else "bias"
+            return f"f_label.{base_idx}.{role}"
+        elif k.startswith("/Parser/birnn/vanilla-lstm-builder"):
+            key = k.split("/Parser/birnn/vanilla-lstm-builder")[1]
+            key = key[1:]
+            birnn_num, param_num = key.split("_")
+            birnn_num = 0 if not birnn_num else int(birnn_num[:-1])
+            param_num = int(param_num)
+
+            layer = birnn_num // 2
+            reverse = "_reverse" if (birnn_num % 2 == 1) else ""
+
+
+            if param_num == 0:
+                return f"lstm.weight_ih_l{layer}{reverse}"
+            elif param_num == 1:
+                return f"lstm.weight_hh_l{layer}{reverse}"
+            elif param_num == 2:
+                return f"lstm.bias_ih_l{layer}{reverse}"
+            else:
+                raise NotImplementedError(f"Key {k} cannot be mapped to pytorch")
+
+            return birnn_num, param_num
+
+        raise NotImplementedError(f"Key {k} cannot be mapped to pytorch")
+
+    state_dict = {}
+    for param in (dynet_parser.model.lookup_parameters_list()
+                    + dynet_parser.model.parameters_list()):
+        name = param.name()
+        torch_name = map_name(name)
+        value = param.as_array()
+        if 'lstm' in torch_name:
+            # reorder the array
+            observed_lstm_size = value.shape[0] // 4
+            value = np.concatenate([
+                value[0*observed_lstm_size:1*observed_lstm_size],
+                value[1*observed_lstm_size:2*observed_lstm_size],
+                value[3*observed_lstm_size:4*observed_lstm_size],
+                value[2*observed_lstm_size:3*observed_lstm_size],
+                ])
+
+        torch_value = torch.from_numpy(value)
+        state_dict[torch_name] = torch_value
+
+        if "bias_ih" in torch_name:
+            second_torch_name = torch_name.replace("bias_ih", "bias_hh")
+            second_torch_value = torch.zeros(torch_value.size())
+            second_torch_value[observed_lstm_size:2*observed_lstm_size] = 1.0
+            state_dict[second_torch_name] = second_torch_value
+
+    return parse_pytorch.ChartParser.from_spec(spec, state_dict)
+
 def run_test(args):
     print("Loading test trees from {}...".format(args.test_path))
     test_treebank = trees.load_trees(args.test_path)
     print("Loaded {:,} test examples.".format(len(test_treebank)))
 
     print("Loading model from {}...".format(args.model_path_base))
-    model = dy.ParameterCollection()
-    [parser] = dy.load(args.model_path_base, model)
+    if args.model_path_base.endswith(".pt"):
+        if not args.use_pytorch:
+            raise NotImplementedError("Can't use pytorch parameter savefiles with dynet backend")
+
+        raise NotImplementedError("Pytorch savefiles not implemented yet")
+    else:
+        model = dy.ParameterCollection()
+        [parser] = dy.load(args.model_path_base, model)
+
+        if args.use_pytorch:
+            parser = dynet_to_pytoch(parser)
 
     print("Parsing test sentences...")
-
     start_time = time.time()
 
     test_predicted = []
     for tree in test_treebank:
-        dy.renew_cg()
+        if not args.use_pytorch:
+            dy.renew_cg()
         sentence = [(leaf.tag, leaf.word) for leaf in tree.leaves()]
         predicted, _ = parser.parse(sentence)
         test_predicted.append(predicted.convert())
@@ -268,6 +374,7 @@ def main():
     subparser.add_argument("--epochs", type=int)
     subparser.add_argument("--checks-per-epoch", type=int, default=4)
     subparser.add_argument("--print-vocabs", action="store_true")
+    subparser.add_argument("--use-pytorch", action="store_true")
 
     subparser = subparsers.add_parser("test")
     subparser.set_defaults(callback=run_test)
@@ -276,6 +383,7 @@ def main():
     subparser.add_argument("--model-path-base", required=True)
     subparser.add_argument("--evalb-dir", default="EVALB/")
     subparser.add_argument("--test-path", default="data/23.auto.clean")
+    subparser.add_argument("--use-pytorch", action="store_true")
 
     args = parser.parse_args()
     args.callback(args)
---
+++ src/parse_pytorch.py
@@ -0,0 +1,191 @@
+import functools
+
+import numpy as np
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.init as init
+
+import trees
+
+START = "<START>"
+STOP = "<STOP>"
+UNK = "<UNK>"
+
+def augment(scores, oracle_index):
+    # NOCUDA
+    increment = torch.ones(scores.size())
+    increment[oracle_index] = 0
+    increment = Variable(increment, requires_grad=False)
+    return scores + increment
+
+class TopDownParser(object):
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("No top-down parser implemented in pytorch")
+
+class ChartParser(nn.Module):
+    # We never actually call forward() end-to-end as is typical for pytorch
+    # modules, but this inheritance brings in good stuff like state dict
+    # management.
+    def __init__(
+            self,
+            tag_vocab,
+            word_vocab,
+            label_vocab,
+            tag_embedding_dim,
+            word_embedding_dim,
+            lstm_layers,
+            lstm_dim,
+            label_hidden_dim,
+            dropout,
+    ):
+        super().__init__()
+        self.spec = locals()
+        self.spec.pop("self")
+
+        self.tag_vocab = tag_vocab
+        self.word_vocab = word_vocab
+        self.label_vocab = label_vocab
+        self.lstm_dim = lstm_dim
+
+        self.tag_embeddings = nn.Embedding(tag_vocab.size, tag_embedding_dim)
+        self.word_embeddings = nn.Embedding(word_vocab.size, word_embedding_dim)
+
+        self.lstm = nn.LSTM(
+            input_size=tag_embedding_dim + word_embedding_dim,
+            hidden_size=lstm_dim,
+            num_layers=lstm_layers,
+            bidirectional=True,
+            batch_first=False)
+
+        self.f_label = nn.Sequential(
+            nn.Linear(2 * lstm_dim, label_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(label_hidden_dim, label_vocab.size - 1),
+            )
+
+    @property
+    def model(self):
+        return self.state_dict()
+
+    @classmethod
+    def from_spec(cls, spec, model):
+        res = cls(**spec)
+        res.load_state_dict(model)
+        return res
+
+    def parse(self, sentence, gold=None):
+        is_train = gold is not None
+        self.train(is_train)
+
+        tag_idxs = np.zeros((len(sentence) + 2, 1), dtype=int)
+        word_idxs = np.zeros_like(tag_idxs)
+
+        for i, (tag, word) in enumerate([(START, START)] + sentence + [(STOP, STOP)]):
+            tag_idxs[i, 0] = self.tag_vocab.index(tag)
+            if word not in (START, STOP):
+                count = self.word_vocab.count(word)
+                if not count or (is_train and np.random.rand() < 1 / (1 + count)):
+                    word = UNK
+            word_idxs[i, 0] = self.word_vocab.index(word)
+
+        tag_idxs = Variable(torch.from_numpy(tag_idxs), requires_grad=False, volatile=not is_train)
+        word_idxs = Variable(torch.from_numpy(word_idxs), requires_grad=False, volatile=not is_train)
+
+        # NOCUDA
+        embeddings = torch.cat([
+            self.tag_embeddings(tag_idxs),
+            self.word_embeddings(word_idxs)
+            ], dim=2)
+
+        lstm_outputs, _ = self.lstm(embeddings)
+        lstm_outputs = lstm_outputs.squeeze(1)
+
+        @functools.lru_cache(maxsize=None)
+        def get_span_encoding(left, right):
+            forward = (
+                lstm_outputs[right][:self.lstm_dim] -
+                lstm_outputs[left][:self.lstm_dim])
+            backward = (
+                lstm_outputs[left + 1][self.lstm_dim:] -
+                lstm_outputs[right + 1][self.lstm_dim:])
+            return torch.cat([forward, backward])
+
+        @functools.lru_cache(maxsize=None)
+        def get_label_scores(left, right):
+            non_empty_label_scores = self.f_label(get_span_encoding(left, right))
+            return torch.cat([Variable(torch.zeros(1)), non_empty_label_scores]) # NOCUDA
+
+        def helper(force_gold):
+            if force_gold:
+                assert is_train
+
+            chart = {}
+
+            for length in range(1, len(sentence) + 1):
+                for left in range(0, len(sentence) + 1 - length):
+                    right = left + length
+
+                    label_scores = get_label_scores(left, right)
+
+                    if is_train:
+                        oracle_label = gold.oracle_label(left, right)
+                        oracle_label_index = self.label_vocab.index(oracle_label)
+
+                    if force_gold:
+                        label = oracle_label
+                        label_score = label_scores[oracle_label_index]
+                    else:
+                        if is_train:
+                            label_scores = augment(label_scores, oracle_label_index)
+                        label_scores_np = label_scores.data.numpy()
+                        argmax_label_index = int(
+                            label_scores_np.argmax() if length < len(sentence) else
+                            label_scores_np[1:].argmax() + 1)
+                        argmax_label = self.label_vocab.value(argmax_label_index)
+                        label = argmax_label
+                        label_score = label_scores[argmax_label_index]
+
+                    if length == 1:
+                        tag, word = sentence[left]
+                        tree = trees.LeafParseNode(left, tag, word)
+                        if label:
+                            tree = trees.InternalParseNode(label, [tree])
+                        chart[left, right] = [tree], label_score
+                        continue
+
+                    if force_gold:
+                        oracle_splits = gold.oracle_splits(left, right)
+                        oracle_split = min(oracle_splits)
+                        best_split = oracle_split
+                    else:
+                        best_split = max(
+                            range(left + 1, right),
+                            key=lambda split:
+                                chart[left, split][1].data[0] +
+                                chart[split, right][1].data[0])
+
+                    left_trees, left_score = chart[left, best_split]
+                    right_trees, right_score = chart[best_split, right]
+
+                    children = left_trees + right_trees
+                    if label:
+                        children = [trees.InternalParseNode(label, children)]
+
+                    chart[left, right] = (
+                        children, label_score + left_score + right_score)
+
+            children, score = chart[0, len(sentence)]
+            assert len(children) == 1
+            return children[0], score
+
+        tree, score = helper(False)
+        if is_train:
+            oracle_tree, oracle_score = helper(True)
+            assert oracle_tree.convert().linearize() == gold.convert().linearize()
+            correct = tree.convert().linearize() == gold.convert().linearize()
+            loss = torch.zeros(1) if correct else score - oracle_score # NOCUDA
+            return tree, loss
+        else:
+            return tree, score
