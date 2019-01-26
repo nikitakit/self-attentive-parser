cc62f28a603da5572376c7f1f55e2b45adeb4145
Nikita Kitaev <nikitakit@gmail.com>
2019-01-25 20:03:28 -0800

    Report exact tree match

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,19 +7,20 @@ import tempfile
 import trees
 
 class FScore(object):
-    def __init__(self, recall, precision, fscore, tagging_accuracy=100):
+    def __init__(self, recall, precision, fscore, complete_match, tagging_accuracy=100):
         self.recall = recall
         self.precision = precision
         self.fscore = fscore
+        self.complete_match = complete_match
         self.tagging_accuracy = tagging_accuracy
 
     def __str__(self):
         if self.tagging_accuracy < 100:
-            return "(Recall={:.2f}, Precision={:.2f}, FScore={:.2f}, TaggingAccuracy={:.2f})".format(
-                self.recall, self.precision, self.fscore, self.tagging_accuracy)
+            return "(Recall={:.2f}, Precision={:.2f}, FScore={:.2f}, CompleteMatch={:.2f}, TaggingAccuracy={:.2f})".format(
+                self.recall, self.precision, self.fscore, self.complete_match, self.tagging_accuracy)
         else:
-            return "(Recall={:.2f}, Precision={:.2f}, FScore={:.2f})".format(
-                self.recall, self.precision, self.fscore, self.tagging_accuracy)
+            return "(Recall={:.2f}, Precision={:.2f}, FScore={:.2f}, CompleteMatch={:.2f})".format(
+                self.recall, self.precision, self.fscore, self.complete_match)
 
 def evalb(evalb_dir, gold_trees, predicted_trees, ref_gold_path=None):
     assert os.path.exists(evalb_dir)
@@ -77,7 +78,7 @@ def evalb(evalb_dir, gold_trees, predicted_trees, ref_gold_path=None):
     )
     subprocess.run(command, shell=True)
 
-    fscore = FScore(math.nan, math.nan, math.nan)
+    fscore = FScore(math.nan, math.nan, math.nan, math.nan)
     with open(output_path) as infile:
         for line in infile:
             match = re.match(r"Bracketing Recall\s+=\s+(\d+\.\d+)", line)
@@ -89,6 +90,9 @@ def evalb(evalb_dir, gold_trees, predicted_trees, ref_gold_path=None):
             match = re.match(r"Bracketing FMeasure\s+=\s+(\d+\.\d+)", line)
             if match:
                 fscore.fscore = float(match.group(1))
+            match = re.match(r"Complete match\s+=\s+(\d+\.\d+)", line)
+            if match:
+                fscore.complete_match = float(match.group(1))
             match = re.match(r"Tagging accuracy\s+=\s+(\d+\.\d+)", line)
             if match:
                 fscore.tagging_accuracy = float(match.group(1))
