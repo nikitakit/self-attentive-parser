a8303307eb189a92aaac8dc09f458d1d29cbc4a8
Nikita Kitaev <nikitakit@gmail.com>
2019-01-31 21:30:49 -0800

    Release v0.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ for ext_module in extensions:
 
 setuptools.setup(
     name="benepar",
-    version="0.1.1",
+    version="0.1.2",
     author="Nikita Kitaev",
     author_email="kitaev@cs.berkeley.edu",
     description="Berkeley Neural Parser",
