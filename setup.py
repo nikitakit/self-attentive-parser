7e892dc909a57c35246e59c54ce105e38fe08c25
Samuel Helms <shelms@Technology-TMBP-2.local>
2019-12-23 20:16:48 -0500

    Added one step install

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+from setuptools import setup, Extension, dist
 import setuptools
 import sys
+
+dist.Distribution().fetch_build_eggs(['Cython>=0.15.1', 'numpy>=1.10'])
 
 try:
     from Cython.Build import cythonize
